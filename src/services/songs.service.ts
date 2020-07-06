8ce0cec1cc918e6f141fc2dd3a9401c4c6c83093
Joshua Amaju <princejosh0007@gmail.com>
2020-07-06 15:57:46 +0100

    add song service helpers

--- src/services/songs.service.ts
+++ src/services/songs.service.ts
@@ -1,7 +1,15 @@
 import { Song } from "./../types";
+import { q, db, Songs } from "../database";
 
-export function addSong(song: Song) {}
+export function addSong(song: Song) {
+  const addQuery = q`CREATE``(song:Song ${song})``[]``()`;
+  return db.exec(addQuery);
+}
 
-export function addSongs(song: Song[]) {}
+export function addSongs(songs: Song[]) {
+  return Promise.all(songs.map(addSong));
+}
 
-// export function getAll(): Song[] {}
+export function getAll(): Promise<Song[]> {
+  return Songs.getAll();
+}
