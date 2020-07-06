3ca851308ef66a7bf258bb53da1815666d20d1e5
Joshua Amaju <princejosh0007@gmail.com>
2020-07-06 15:57:01 +0100

    add album art

--- src/components/SongTile.tsx
+++ src/components/SongTile.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Song } from "../types";
+import AlbumArt from "./AlbumArt";
 
 interface SongTile {
   song: Song;
+  onClick?: React.ReactEventHandler;
 }
 
-export default function SongTile({ song }: SongTile) {
-  const { title, artist, getImage } = song;
-  const description = `${artist} - ${title}`;
+export default function SongTile({ song, onClick }: SongTile) {
+  const { title, artist } = song;
 
   return (
-    <div>
-      <img src={getImage()} alt={description} />
+    <div onClick={onClick}>
+      <AlbumArt song={song} />
       <div>
         <h2>{title}</h2>
         <h3>{artist}</h3>
