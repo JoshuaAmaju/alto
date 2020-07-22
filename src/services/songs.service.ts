c6dacdc343e041c8c615f9c8e8ea486d4cc440ba
Joshua Amaju <princejosh0007@gmail.com>
2020-07-22 18:57:22 +0100

    fix dom ipc exception

--- src/services/songs.service.ts
+++ src/services/songs.service.ts
@@ -1,8 +1,10 @@
+import { db, q } from "../database";
 import { Song } from "./../types";
-import { q, db, Songs } from "../database";
 
-export function getAll() {
-  return Songs.getAll();
+export async function getAll(): Promise<Song[]> {
+  const query = q`MATCH``(song:Song)``[]``()`;
+  const res = await db.exec(query, { return: "song" });
+  return (res as Record<string, unknown>[]).map(({ song }) => song) as Song[];
 }
 
 export function addSong(song: Song) {
