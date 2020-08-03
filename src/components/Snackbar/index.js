41202ba8d55ce56a383ad0ffa29ec3fedb0f44e6
Joshua Amaju <princejosh0007@gmail.com>
2020-08-03 16:12:08 +0100

    add snackbar/notification component

---
+++ src/components/Snackbar/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {style} from './index.scss';
+
+const Snackbar = ({}) => {
+
+    return (
+        <div className={style}>
+            
+        </div>
+    );
+};
+
+Snackbar.propTypes = {
+
+};
+
+export default Snackbar;
