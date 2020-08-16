10a99ed242fdedf18b2aefcbef90bd86829fb700
Joshua Amaju <princejosh0007@gmail.com>
2020-08-16 13:17:07 +0100

    replace framer motion utilities with native implementation

--- src/screens/Playlist.tsx
+++ src/screens/Playlist.tsx
@@ -1,17 +1,24 @@
-import { Color, Frame, useMotionValue, useTransform } from "framer";
-import { AnimatePresence, motion } from "framer-motion";
+import { Color } from "framer";
+import {
+  AnimatePresence,
+  motion,
+  useMotionValue,
+  useTransform,
+} from "framer-motion";
 import React, {
   createRef,
-  memo,
   RefObject,
+  useEffect,
   useLayoutEffect,
   useMemo,
+  useRef,
   useState,
 } from "react";
 import { Play, Shuffle } from "react-feather";
 import { createUseStyles } from "react-jss";
-import { useHistory, useParams } from "react-router-dom";
 import { usePalette } from "react-palette";
+import { useHistory, useParams } from "react-router-dom";
+import { useScroll } from "react-use";
 import AlbumArt from "../components/AlbumArt";
 import Button from "../components/Button";
 import FlatButton from "../components/FlatButton";
@@ -23,6 +30,10 @@ import { ShuffleMode } from "../QueueService/types";
 import { Song } from "../types";
 
 const useStyle = createUseStyles({
+  sticky: {
+    top: 0,
+    position: "sticky",
+  },
   frame: {
     display: "flex",
     padding: "2rem 1.5rem",
@@ -40,8 +51,9 @@ const useStyle = createUseStyles({
     boxShadow: ({ color }) => `0px 50px 20px -40px ${color}`,
   },
   main: {
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
+    zIndex: 1,
+    overflow: "hidden",
+    position: "relative",
   },
   button: {
     display: "flex",
@@ -81,11 +93,12 @@ function Playlist() {
   const { name } = useParams();
   const { goBack } = useHistory();
 
-  const ref = createRef<HTMLDivElement>();
-  const ref2 = createRef<HTMLDivElement>();
-  const { top, height } = useDOMRect(ref);
+  const heroRef = createRef<HTMLDivElement>();
+  const pageRef = useRef<HTMLDivElement>(null);
+  const listWrapperRef = createRef<HTMLDivElement>();
 
-  const rect = useDOMRect(ref2);
+  const rect = useDOMRect(heroRef);
+  const { top } = useDOMRect(listWrapperRef);
 
   const { playlistsMap } = usePlaylists();
   const [queueOpen, setQueueOpen] = useState(false);
@@ -99,19 +112,17 @@ function Playlist() {
     return Math.max(bottom, height) - Math.min(bottom, height);
   }, [rect]);
 
-  // This calculates the available scrollable space of
-  // the songs list for adding constraint on y axis during
-  // a drag in the upward direction.
-  const diff = useMemo(() => {
-    const topHeight = top + height;
-    return window.innerHeight - topHeight;
-  }, [top, height]);
+  const { y: scrollY } = useScroll(pageRef);
 
-  const y = useMotionValue<any>(0);
-  const alpha = useTransform<any>(y, [0, -ratio], [0, 1]);
-  const scale = useTransform<any>(y, [0, top, top + 100], [1, 1.5, 2]);
+  const y = useMotionValue(scrollY);
+  const radius = useTransform(y, [0, ratio], [0, 20]);
+  const scale = useTransform(y, [0, -(top + 100)], [1, 2]);
 
-  const radius = useTransform<any>(alpha, (v) => v * 12);
+  const backgroundColor = useTransform(
+    y,
+    [0, ratio],
+    ["rgba(255, 255, 255, 0)", "rgba(255, 255, 255, 1)"]
+  );
 
   const { label, songs, coverUrl } = useMemo(() => {
     return playlistsMap[name] ?? {};
@@ -127,119 +138,104 @@ function Playlist() {
 
   let color = Color.alpha(Color(vibrant), 0.4);
 
-  const classes = useStyle({
-    muted,
-    color: color.toValue(),
-  });
+  const classes = useStyle({ muted, color: color.toValue() });
 
   const open = (shuffle?: ShuffleMode) => {
     openQueue(songs as Song[], shuffle);
   };
 
+  useEffect(() => {
+    y.set(scrollY);
+  }, [y, scrollY]);
+
   return (
-    <div className="Page">
-      <div className={classes.header}>
-        <FlatButton onClick={goBack}>
-          <AnimatePresence>
-            <motion.svg
-              width={35}
-              height={35}
-              fill="none"
-              stroke-width="2"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              exit={{ x: -30, opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              initial={{ x: -30, opacity: 0 }}
-              xmlns="http://www.w3.org/2000/svg"
-              transition={{ damping: 15, type: "spring" }}
+    <div className="Page" ref={pageRef}>
+      <div className={classes.sticky}>
+        <div className={classes.header}>
+          <FlatButton onClick={goBack}>
+            <AnimatePresence>
+              <motion.svg
+                width={35}
+                height={35}
+                fill="none"
+                stroke-width="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                exit={{ x: -30, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                initial={{ x: -30, opacity: 0 }}
+                xmlns="http://www.w3.org/2000/svg"
+                transition={{ damping: 15, type: "spring" }}
+              >
+                <polyline points="15 18 9 12 15 6"></polyline>
+              </motion.svg>
+            </AnimatePresence>
+            <motion.h2 layoutId="playlists">Playlists</motion.h2>
+          </FlatButton>
+        </div>
+        <div ref={heroRef} className={classes.frame}>
+          <div style={{ position: "relative" }}>
+            <AlbumArt
+              url={coverUrl}
+              layoutId={name}
+              style={{ scale }}
+              className={classes.cover}
+            />
+            <FlatButton
+              style={{
+                top: 20,
+                left: 20,
+                width: "2.5rem",
+                height: "2.5rem",
+                padding: "0.7rem",
+                borderRadius: 100,
+                position: "absolute",
+                backdropFilter: "blur(20px)",
+                backgroundColor: "#ffffffc2",
+              }}
+              onClick={() => {
+                if (!queueOpen) open();
+                playSongAt(0);
+                setQueueOpen(true);
+              }}
             >
-              <polyline points="15 18 9 12 15 6"></polyline>
-            </motion.svg>
-          </AnimatePresence>
-          <motion.h2 layoutId="playlists">Playlists</motion.h2>
-        </FlatButton>
-      </div>
-      <div ref={ref2} className={classes.frame}>
-        <div style={{ position: "relative" }}>
-          <AlbumArt
-            url={coverUrl}
-            layoutId={name}
-            style={{ scale }}
-            className={classes.cover}
-          />
-          <FlatButton
-            style={{
-              top: 20,
-              left: 20,
-              width: "2.5rem",
-              height: "2.5rem",
-              padding: "0.7rem",
-              borderRadius: 100,
-              position: "absolute",
-              backdropFilter: "blur(20px)",
-              backgroundColor: "#ffffffc2",
-            }}
+              <Play size="100%" stroke="none" fill="#fff" />
+            </FlatButton>
+          </div>
+
+          <div>
+            <Text variant="h2">{name}</Text>
+            <Text variant="h4">{label}</Text>
+          </div>
+
+          <Button
+            className={classes.button}
             onClick={() => {
-              if (!queueOpen) open();
+              open(ShuffleMode.SHUFFLE);
               playSongAt(0);
               setQueueOpen(true);
             }}
           >
-            <Play size="100%" stroke="none" fill="#fff" />
-          </FlatButton>
+            <span>Shuffle Play</span>
+            <Shuffle size={20} />
+          </Button>
         </div>
-
-        <div>
-          <Text variant="h2">{name}</Text>
-          <Text variant="h4">{label}</Text>
-        </div>
-
-        <Button
-          className={classes.button}
-          onClick={() => {
-            open(ShuffleMode.SHUFFLE);
-            playSongAt(0);
-            setQueueOpen(true);
-          }}
-        >
-          <span>Shuffle Play</span>
-          <Shuffle size={20} />
-        </Button>
       </div>
-      <Frame
-        y={y}
-        drag="y"
-        ref={ref}
-        radius={20}
-        width="100%"
-        height="auto"
-        dragElastic={0.2}
-        position="relative"
-        backgroundColor="none"
+      <motion.div
+        ref={listWrapperRef}
         className={classes.main}
-        dragConstraints={{
-          bottom: 0,
-          top: diff < 0 ? diff : 0,
-          // top: -(
-          //   Math.max(window.innerHeight, topHeight) -
-          //   Math.min(window.innerHeight, topHeight)
-          // ),
+        style={{
+          backgroundColor,
+          borderTopLeftRadius: radius,
+          borderTopRightRadius: radius,
         }}
       >
-        <Frame
-          size="100%"
-          opacity={alpha}
-          radius={radius}
-          style={{ zIndex: -1 }}
-          backgroundColor="white"
-        />
         {songs && <SongsList songs={songs} color={muted} />}
-      </Frame>
+      </motion.div>
     </div>
   );
 }
 
-export default memo(Playlist);
+export default Playlist;
